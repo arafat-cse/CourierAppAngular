f09f18255e15d9e4a6545842b2b4858e1dc7085a
Arafat <arafat.developer2008@gmail.com>
2024-12-26 01:18:22 +0600

    ParcelType Create

---
+++ src/app/security/parcel-type/parcel-type.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parcel-type',
+  standalone: true,
+  imports: [],
+  templateUrl: './parcel-type.component.html',
+  styleUrl: './parcel-type.component.css'
+})
+export class ParcelTypeComponent {
+
+}
